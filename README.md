834a87a58686509d49a0ed02f2b4f9c95ed6c5ed
Mano Deepan <manodeepan2001@gmail.com>
2024-05-21 19:33:20 +0530

    nodejs-filesystem task added

---
+++ README.md
@@ -0,0 +1,42 @@
+## NodeJS File System
+
+**Simple Node.js File System App**
+
+This Node.js application allows you to create and view text files with timestamps. 
+
+It features:
+- File creation with timestamps
+- Text file content viewing
+- User-friendly interface with routes for:
+    - Creating files
+    - Viewing all files
+
+## Installation and Setup
+
+1. Clone the repository:
+```
+git clone https://github.com/your-username/your-repo-name.git
+
+cd your-repo-name
+```
+
+2. Install dependencies:
+```
+npm install express nodemon date-fns
+```
+
+3. Start the development server:
+```
+npm run dev
+```
+
+The server will start on port `3000` by default. You can access the application routes in your browser:
+
+- Create a new text file:  http://localhost:3000/create
+- View all files:  http://localhost:3000/view
+
+Technologies Used:
+- Node.js
+- Express.js
+- `fs` module(for file system operation)
+- `date-fns` module (for date formatting)
---
+++ src/app.js
@@ -0,0 +1,128 @@
+import express from "express";
+import { format } from 'date-fns';
+import fs from 'fs/promises'
+
+const app = express();
+const port = 3000;
+
+// Styles for various HTML elements
+const divStyle = `
+    height:100%; 
+    font-family:cursive; 
+    display:flex; 
+    flex-direction:column; 
+    justify-content:center; 
+    align-items:center
+`;
+
+const achorStyle = `
+    text-decoration:none; 
+    color: #000; 
+    border: 1px solid; 
+    padding: 5px; 
+    border-radius: 5px
+`;
+
+const tableBorder = `
+    border: 1px solid black; 
+    border-collapse: collapse; 
+    padding: 15px
+`;
+
+// Function to generate the home page content
+const HomePage = (text) => {
+    const home = `<div style= "${divStyle}">
+        <h1>${text}</h1>
+        <h3 style="color:green">Server running in port: ${port}<h3>
+        <a href="/create" style="${achorStyle}">
+            To Create text file
+        </a>
+        <p><a href="/view" style="${achorStyle}">
+            To View all the text file
+        </a></p>
+    </div`
+
+    return home;
+}
+
+// Function to generate the create page content
+const createPage = (formateDate, data) => {
+    const create = `<div style="${divStyle}">
+        <h1 style = "color: green">File created successfully!</h1>
+        <h2>Text file name: ${formateDate}.txt</f2>    
+        <h3>${data}</h3>
+        <hr/>
+        <p><a href="/" style="${achorStyle}">Home Page</a><p>
+    </div>`
+    return create
+}
+
+// Function to generate the view page content
+const viewPage = async (textFile) => {
+    const tableRows = await Promise.all(textFile.map(async(file, index) => {
+        const fileContent = await fs.readFile(`./time-stamp/${file}`,"utf8");
+        return `
+        <tr key = ${index}>
+            <td style = "${tableBorder}">${file}</td>
+            <td style = "${tableBorder}">${fileContent}</td>
+        </tr>
+        `;
+    }));
+
+    // Join the table rows using an empty string as the separator
+    const tableBody = tableRows.join('')
+
+    return `
+    <div style= "${divStyle}">
+        <table style = "${tableBorder}">
+            <tr>
+                <th style = "${tableBorder}">File Name</th>
+                <th style = "${tableBorder}">Content</th>
+            </tr>
+            ${tableBody}
+        </table>
+        <p><a href="/" style="${achorStyle}">Home Page</a><p>
+    </div>
+    `
+}
+
+// Function to generate error message
+const textError = (error) => {
+    const err = `<div style = "${divStyle}">
+        <p style = "color: red">${error}<p>
+    </div>`
+    return err;
+}
+
+
+// Route handlers
+app.get("/", (req, res) => res.status(200).send(HomePage("Node.js File system")))
+
+app.get("/create",async(req, res) => {
+    try {
+        const formateDate = format(new Date(), "dd-MM-yyyy-HH-mm-ss");
+        const filePath = `./time-stamp/${formateDate}.txt`;
+        const date = `Current timestamp: ${formateDate}`
+
+        fs.writeFile(filePath, date, "utf8")
+        let content = await fs.readFile(filePath, "utf8")
+
+        res.status(200).send(createPage(formateDate, content))
+    }catch(err)
+    {
+        res.send(textError(err.message))
+    }  
+})
+
+app.get("/view",async(req, res) => {
+    try {
+        // Get all the files from the time-stamp directory
+        const textFiles = await fs.readdir("./time-stamp")
+        res.status(200).send(await viewPage(textFiles))
+    }catch(err)
+    {
+        res.send(textError(err.message))
+    } 
+})
+
+app.listen(port, () => console.log(`server running in ${port}`))
